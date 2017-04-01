5977279f3fed06de290d9d3859f3a6fe9216023e
Nick Jenkins <nickjenkinsweb@gmail.com>
2017-04-01 16:02:56 -0700

    Beginning pivot

--- spec/burble.spec.js
+++ spec/burble.spec.js
@@ -1,7 +1,16 @@
-import {burble} from '../src'
+"use strict";
+
+import burble from '../src'
 
 describe('burble', () => {
-  it('should be a function', () => {
-    expect(typeof burble).toBe('function');
+  it('should work', () => {
+    let b = burble(['a', 'b', 'c']);
+    expect(typeof b.next).toBe('function');
+
+    let output = [];
+    for(let v of b){
+      output.push(v);
+    }
+    expect(output[2]).toBe('c');
   });
 });
