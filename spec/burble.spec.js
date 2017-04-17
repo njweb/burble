12ccd20edf87ace5e5b409741a6f857a020f44fd
Nick Jenkins <nickjenkinsweb@gmail.com>
2017-04-17 08:06:23 -0700

    Progress

--- spec/burble.spec.js
+++ spec/burble.spec.js
@@ -1,56 +1,63 @@
 "use strict";
 
+const path = require('path');
+const mockFn = jest.fn();
 import burble from '../src'
+import {parseFile} from '../src'
+import faker from 'faker'
+
+const maybe = (valueA, valueB, probability = 0.5) => {
+  if (Math.random() > probability) return valueB;
+  else return valueA;
+};
+
+const probably = (valueA, valueB, probability = 0.9) => {
+  if (Math.random() > probability) return valueB;
+  else return valueA;
+};
+
+const factoryToGenerator = function (factory) {
+  return function*() {
+    while (true) {
+      yield factory();
+    }
+  }
+};
+
+const userFactory = () => {
+  return {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    friendlyName: maybe(faker.name.firstName(), '')
+  }
+};
+
+const nameLength = (user) => {
+  if (user.hasOwnProperty('friendlyName')) return user.friendlyName.length;
+  else return user.firstName.length + user.lastName.length + 1;
+};
 
 describe('burble', () => {
-  describe('approach', () => {
-
-
-    // it('should generate', () => {
-    //   let nestedGenerator = function*() {
-    //     for (let arr of [[1, 2, 3], [5, 6, 7]]) {
-    //       yield* arr;
-    //     }
-    //     // yield* [1, 2, 3];
-    //     // yield* [5, 6, 7];
-    //   };
-    //
-    //   return new Promise(res => {
-    //     console.log('HERE');
-    //     for (let v of nestedGenerator()) {
-    //       expect(Number.isInteger(v)).toBe(true);
-    //     }
-    //     res();
-    //   });
-    // });
-    // it('should work', () => {
-    //   let p = Promise.resolve(5);
-    //   return p.then(v => expect(v).toBe(5));
-    // });
-
-    // it('should work -> ', () => {
-    //   return new Promise((res) => {
-    //     setTimeout(() => res(5), 100);
-    //   }).then(value => {
-    //     it('right here ', () => {
-    //       expect(value).toBe(6)
-    //     });
-    //   });
-    // });
-
-    describe('should work with ', () => {
-      return new Promise(res => {
-        burble(source => {
-          for (let value of source()) {
-            console.log('HERE', typeof value);
-            it(`${value}`, () => {
-              expect(typeof value).toBe('string');
-            });
-          }
-        }, ['a', 'b', 'c', 1]);
-        res();
-      });
-    });
-
+  return burble({
+    examples: ['a', 'b'],
+    iterator: factoryToGenerator(() => faker.random.alphaNumeric(1))(),
+    filename: path.resolve(__dirname, './examples.json') + '@things'
+  })('should work', (item) => {
+    console.log('TEST: ', item);
+    expect(item.length < 4).toBe(true);
   });
+  // it('should work', () => {
+  //   console.log("PATH -> ", path.resolve(__dirname, './examples.json'));
+  //   // return parseFile(path.resolve(__dirname, './examples.json') + '@things');
+  //   return parseFile(path.resolve(__dirname, './examples.json') + '@things');
+  // });
+
+  // burble({
+  //   examples: [userFactory(), userFactory()],
+  //   iterator: factoryToGenerator(userFactory)()
+  // })('should work', (userValue) => {
+  //   expect(nameLength(userValue) > 0).toBe(true);
+  // });
+
+
 });
--- src/index.js
+++ src/index.js
@@ -1,26 +1,81 @@
 require('regenerator-runtime/runtime');
+import fs from 'mz/fs'
 
+const labelString = (labelRoot) => labelRoot + ' with: ';
 
-const burble = function (testingHandler) {
-  const parseArgsTail = (args) => {
-    let argsArray = [];
-    for (let i = 1; i < args.length; i += 1) {
-      argsArray.push(args[i]);
-    }
-    return argsArray;
-  };
-
-  const generatorArgs = parseArgsTail(arguments);
-
-  return new Promise((res, rej) => {
-    testingHandler(function *() {
-      for (let gen of generatorArgs) {
-        console.log('GEN: ', gen);
-        yield* gen;
+export const parseFile = (filename) => {
+  let parts = filename.split('@');
+  console.log("HERE", parts[0]);
+  return fs.exists(parts[0])
+    .then((result) => {
+      console.log('HERE A');
+      if (!result) throw Error('File not found');
+      return fs.readFile(parts[0], 'utf8');
+    }).then((data) => {
+      console.log("HERE B");
+      data = JSON.parse(data);
+      if (parts[1]) {
+        const key = parts[1];
+        if (!data.hasOwnProperty(key)) throw Error('Examples are missing requested property');
+        if (!Array.isArray(data[key])) throw Error('Examples must be stored in an array');
+        return data[key];
+      } else {
+        if (!Array.isArray(data)) throw Error('Examples must be stored in an array');
+        return data;
       }
-      res();
     });
-  });
+};
+
+const wrapTest = (test, element, callback) => {
+  try {
+    test(element);
+    callback();
+  } catch (e) {
+    callback(e);
+  }
+};
+
+const burble = ({examples, iterator, filename, iteratorCycles = 10}) => {
+  if (!examples && !iterator && !filename) throw Error('Burble needs at least one example source');
+
+  return (description, test) => {
+    return Promise.resolve((res => {
+      if (examples) {
+        console.log("EXAMPLES");
+        examples.forEach(e => it(description, test.bind(null, e)));
+      }
+    })()).then(() => {
+      if (filename) {
+        console.log("FILENAME", filename);
+        return parseFile(filename)
+          .then(examples => {
+            console.log("E ------> ", examples, Array.isArray(examples));
+            examples.forEach(e => {
+              console.log("HERE C", e);
+              it(description, test.bind(null, e));
+            });
+          });
+      }
+    }).then(() => {
+      if (iterator) {
+        console.log("ITERATOR");
+        let abort = false;
+        let counter = 0;
+        let item = iterator.next();
+        while (counter++ < iteratorCycles && !item.done && !abort) {
+          wrapTest(test, item.value, e => {
+            if (e) {
+              it(description, () => {
+                throw e;
+              });
+              abort = true;
+            }
+          });
+          item = iterator.next();
+        }
+      }
+    });
+  }
 };
 
 export default burble;
