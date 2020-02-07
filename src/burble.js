51f3f2f3ee914915ace6df314a81016e29e2a864
nick jenkins <nickjenkinsweb@gmail.com>
2020-02-06 17:34:12 -0800

    remove broken default export from main file

--- src/burble.js
+++ src/burble.js
@@ -1,6 +1,2 @@
 export { default as actionArrayMiddleware } from './actionArrayMiddleware.js';
 export { default as autoReducer } from './autoReducer.js';
-export default {
-  actionArrayMiddleware,
-  autoReducer,
-};
